fa26317bff5dd7955e39cdc4d40f9c539ffc030b
CharlesMich <charleedongo@hotmail.com>
2025-12-23 13:07:18 -0600

    Initial Commit

---
+++ freedom_rosary/freedom_rosaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  freedom_rosaryApp.swift
+//  freedom_rosary
+//
+//  Created by Charles Michael on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct freedom_rosaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
